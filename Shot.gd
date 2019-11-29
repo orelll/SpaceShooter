d43fb115d44d67cdaeddb9d37a6fdf495644df82
Szymon Orlik <orlikszym@gmail.com>
2019-11-29 16:56:45 +0100

    prepared for lesson

--- Shot.gd
+++ Shot.gd
@@ -19,29 +19,3 @@ func _physics_process(delta):
 		$AnimatedSprite.play("explosion")
 		yield($AnimatedSprite, "animation_finished" )
 		queue_free()
-
-func _on_Shot2_area_entered(area):
-	if area.get_name() == 'target':
-		velocity = Vector2(0,0)
-		$AnimatedSprite.play("explosion")
-		yield($AnimatedSprite, "animation_finished" )
-		var target = find_node_by_name(get_tree().get_root(), "target")
-		find_node_by_name(get_tree().get_root(), 'root').call_deferred("remove_child",target) 
-		
-		var player = find_node_by_name(get_tree().get_root(), "Player")
-		player.spawn_target()
-		get_parent().remove_child(self)
-
-func find_node_by_name(root, name):
-
-    if(root.get_name() == name): return root
-
-    for child in root.get_children():
-        if(child.get_name() == name):
-            return child
-
-        var found = find_node_by_name(child, name)
-
-        if(found): return found
-
-    return null
